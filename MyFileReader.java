da45066b4fff846082b013d07c6002fb2d805166
Yubin <yubinkim57@gmail.com>
2015-05-27 13:16:20 -0400

    initial commit file reader

---
+++ MyFileReader.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class MyFileReader{ //needed to read data and questions
+
+    private static final String questions = "questions.txt";
+    private static final String data = "data.txt";    
+
+    public int[] questionList;
+    public int [][] dataSet;
+
+    public MyFileReader(String fileName){
+	
+		
+    }
+	
+    
+    public void read(){
+	
+	
+    }
+    
+
+    public void readQuestion(){
+
+    }
+
+
+
+    public void readData(){
+
+
+    }
+    
+
+
+
+
+
+}
