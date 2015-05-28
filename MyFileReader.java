cf28fe8c090d16ed0b0759a68746023ffa57a8d5
Yubin <yubinkim57@gmail.com>
2015-05-28 10:57:28 -0400

    myfilereader done except filereaderfoundexception error WHATTTTT

--- MyFileReader.java
+++ MyFileReader.java
@@ -8,7 +8,7 @@ public class MyFileReader{ //needed to read data and questions
     private static final String dataFile = "data.txt";
 
     private String[] questions, teachers;
-    private int[][] data;
+    private char[][] data; 
     private String filePath;
 
     private boolean isArray;
@@ -22,9 +22,31 @@ public class MyFileReader{ //needed to read data and questions
 	    isArray = false;
 	}
 
+	try{
+
+	    int size1 = 0;
+	    int size2 = 0;
+
+	    Scanner in = new Scanner(fileName);
+	    while(in.hasNextLine()){
+		size1++;
+	    }
+
+	    if(isArray){
+		if(fileName.equals(questionFile)){
+		    questions = new String[size1];
+		}else{
+		    teachers = new String[size1];   
+		}
+	    }else{
+		String str = in.nextLine();
+		size2 = (int) str.length();
+		data = new char[size1][size2];
+	    }
+	}catch(FileNotFoundException e){}
+	
+		   
 	filePath = fileName;
-
-
 		
     }
 	
@@ -36,41 +58,62 @@ public class MyFileReader{ //needed to read data and questions
        
 	
 	}
+	catch(FileNotFoundException e){}
     }
     
 
-    public int[] QuestionArray(){
+    public String[] QuestionArray(){
 
 	try{
 	    Scanner in = new Scanner(filePath);
-	    int size = 0;
-
-	    while(in.hasNextLine()){
-	        size++;
-	    }
-
-	    questions = new Stirng[size];
-
-	    for(int i = 0; i < size; i++){
-		question[i] = in.nextLine();
+	   
+	    for(int i = 0; i < questions.length; i++){
+		String q = in.nextLine();
+		questions[i] = q;
 	    }
 	    
-	}
-	catch(FileNotFoundException e){	}
+	}catch(FileNotFoundException e){ }
+
+	return questions;
 
     }
 
 
-    public int[] TeacherArray(){
+    public String[] TeacherArray(){
 
+	try{
+	    Scanner in = new Scanner(filePath);
 
+	    for(int i=0; i<teachers.length; i++){
+		String q = in.nextLine();
+		teachers[i] = q;
+
+	    }
+
+	}catch(FileNotFoundException e){}
+
+	return teachers;
 
     }
 
-    public int[][] DataArray(){
+    public char[][] DataArray(){
 
+	try{
+	    Scanner in = new Scanner(filePath);
+
+	    for(int i=0; i<data.length; i++){
+		String answer = in.nextLine();
+		for(int j=0; j< (int)data[0].length/2; j++){
+		    char ans =  answer.charAt(j *2);
+		    data[i][j] = ans;
+		}
+		
+	    }
+
+	}catch(FileNotFoundException e){}
+
+	return data;
     }
-
 
 
 }
