69cda8ac6bf9e4535a1f2d1c1b7a7f6a8339bd8d
Yubin <yubinkim57@gmail.com>
2015-05-28 08:41:34 -0400

    file reader question array

--- MyFileReader.java
+++ MyFileReader.java
@@ -3,37 +3,73 @@ import java.io.*;
 
 public class MyFileReader{ //needed to read data and questions
 
-    private static final String questions = "questions.txt";
-    private static final String data = "data.txt";    
+    private static final String questionFile = "questions.txt";
+    private static final String teacherFile = "teachers.txt";
+    private static final String dataFile = "data.txt";
 
-    public int[] questionList;
-    public int [][] dataSet;
+    private String[] questions, teachers;
+    private int[][] data;
+    private String filePath;
+
+    private boolean isArray;
+
 
     public MyFileReader(String fileName){
-	
+	if(fileName.equals(questionFile) || fileName.equals(teacherFile)){
+	    isArray = true;
+	}
+	if(fileName.equals(dataFile)){
+	    isArray = false;
+	}
+
+	filePath = fileName;
+
+
 		
     }
 	
-    
+
+    //scans through file    
     public void read(){
+	try{
+	Scanner in = new Scanner (filePath);
+       
 	
-	
+	}
     }
     
 
-    public void readQuestion(){
+    public int[] QuestionArray(){
+
+	try{
+	    Scanner in = new Scanner(filePath);
+	    int size = 0;
+
+	    while(in.hasNextLine()){
+	        size++;
+	    }
+
+	    questions = new Stirng[size];
+
+	    for(int i = 0; i < size; i++){
+		question[i] = in.nextLine();
+	    }
+	    
+	}
+	catch(FileNotFoundException e){	}
 
     }
 
 
+    public int[] TeacherArray(){
 
-    public void readData(){
 
 
     }
-    
 
+    public int[][] DataArray(){
 
+    }
 
 
 
