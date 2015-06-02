afbb6dfb5878a66a6a0879ef39b4fe21948675ab
Yubin <yubinkim57@gmail.com>
2015-06-02 08:49:59 -0400

    COMPLETELY FINISHED WITH FILEREADER

--- TestFileReader.java
+++ TestFileReader.java
@@ -18,6 +18,18 @@ public class TestFileReader{
 	System.out.println(Arrays.toString(teacher));
 
 
+	char[][] data;
+	data= MyFileReader.getDataArray("data.txt");
+
+	for(int i =0 ; i < data.length; i++){
+	    String str = "";
+	    for(int j = 0; j <data[0].length; j++){
+
+		str += data[i][j];
+
+	    }
+	    System.out.println(str);
+	}
 
 
     }
