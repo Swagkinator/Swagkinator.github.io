c37ee2c81ccd17b72786a53b5fac930d83fc8276
Yubin <yubinkim57@gmail.com>
2015-06-01 20:11:55 -0400

    blahhhhh why no work

--- UserInterface.java
+++ UserInterface.java
@@ -7,25 +7,46 @@ import java.awt.*;
 public class UserInterface extends JFrame{
 
     private Container pane;
+    private JLabel swaginator;
     private JButton yesbutton, nobutton, idkbutton;
 
     public UserInterface(){
+
+	pane = this.getContentPane();
+
+
+	GroupLayout layout = new GroupLayout(pane);
 
 	this.setTitle("Swagkinator!");
 	this.setSize(600,100);
 	this.setLocation(100,100);
 	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	pane = this.getContentPane();
-	pane.setLayout(new FlowLayout());
+	pane.setLayout(layout);
 
-	yesbutton = new JButton("YES!");
-	nobutton = new JButton("NO!");
+	layout.setAutoCreateGaps(true);
+	layout.setAutoCreateContainerGaps(true);
+
+	//swagkinator = new JLabel("Swag-kinator says:");
+	yesbutton = new JButton("YES");
+	nobutton = new JButton("NO");
 	idkbutton = new JButton("I DON'T KNOW");
+	
+	
+	layout.setVerticalGroup(
+				layout.createSequentialGroup()
+				.addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+					  .addComponent(yesbutton)
+					  .addComponent(nobutton)
+					  .addComponent(idkbutton)
+					  )
+				);
 
-	pane.add(yesbutton);
-	pane.add(nobutton);
-	pane.add(idkbutton);
+
+	//pane.add(swagkinator);
+      	//pane.add(yesbutton);
+	//pane.add(nobutton);
+	//pane.add(idkbutton);
     }
 
 
