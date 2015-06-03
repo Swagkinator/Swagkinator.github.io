085c85821c21d258bcde29db40b8609c8d576850
Yubin <yubinkim57@gmail.com>
2015-06-03 12:27:49 -0400

    blabhblahblabhalh

---
+++ akinator.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.awt.*;
+import java.event.*;
+import javax.swing.*;
+import java.io.*;
+
+public class akinator{
+
+
+
+    public static void main(String[] args){
+
+	//creates new user interface
+	UserInterface akinator = new UserInterface(1);
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
