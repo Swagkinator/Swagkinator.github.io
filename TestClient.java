c7d7e7f76cf96e86232d2f39183910c58d651096
Jion Fairchild <jijiglobe@yahoo.com>
2015-06-04 20:39:56 -0400

    fix a bug with sending multi-line files

--- TestClient.java
+++ TestClient.java
@@ -1,8 +1,8 @@
 public class TestClient{
     public static void main(String[]args){
 	Relay myRelay = new Relay();
-	myRelay.writeToFile("FILENAME","If you are reading this, my code works");
+	myRelay.writeToFile("FILENAME","If you are reading this,\n my code works");
 	System.out.println(myRelay.retrieve("FILENAME"));
-	System.out.println(myRelay.retrieve("FILENAME"));
+	//System.out.println(myRelay.retrieve("FILENAME"));
     }
 }
